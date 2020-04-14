796b752c309a6db13bd15f5431dca4c514b29e12
TomPretty <tompretty2@gmail.com>
2020-04-14 12:31:12 +0100

    Refactor parser into a class

--- calculator/parse.py
+++ calculator/parse.py
@@ -2,4 +2,12 @@ from .expression import ConstExpr
 
 
 def parse(string):
-    return ConstExpr(value=int(string))
+    return Parser(string).parse()
+
+
+class Parser:
+    def __init__(self, string):
+        self.string = string
+
+    def parse(self):
+        return ConstExpr(value=int(self.string))
