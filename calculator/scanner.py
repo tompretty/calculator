da0769a1b74913c11390cdfc04f380dbaa702484
TomPretty <tompretty2@gmail.com>
2020-04-15 16:00:48 +0100

    Add a simple scanner
    Currently this isn't used by the parser. This change will be made
    in the next PR.

---
+++ calculator/scanner.py
@@ -0,0 +1,82 @@
+from enum import Enum
+
+
+class TokenType(Enum):
+    PLUS = 1
+    MINUS = 2
+    TIMES = 3
+    DIVIDE = 4
+    LEFT_PAREN = 5
+    RIGHT_PAREN = 6
+    NUMBER = 7
+
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+
+
+class Scanner:
+    def __init__(self, source):
+        self.source = source
+        self.tokens = []
+        self.start = 0
+        self.current = 0
+
+    def scan_tokens(self):
+        while not self._is_at_end():
+            self._scan_token()
+
+        return self.tokens
+
+    def _scan_token(self):
+        self.start = self.current
+
+        c = self._advance()
+        if c == "+":
+            self._add_token(TokenType.PLUS)
+        elif c == "-":
+            self._add_token(TokenType.MINUS)
+        elif c == "*":
+            self._add_token(TokenType.TIMES)
+        elif c == "/":
+            self._add_token(TokenType.DIVIDE)
+        elif c == "(":
+            self._add_token(TokenType.LEFT_PAREN)
+        elif c == ")":
+            self._add_token(TokenType.RIGHT_PAREN)
+        elif c.isdigit():
+            self._number()
+
+    def _number(self):
+        while self._peek().isdigit():
+            self._advance()
+
+        if self._peek() == "." and self._peek_next().isdigit():
+            self._advance()
+
+            while self._peek().isdigit():
+                self._advance()
+
+        self._add_token(TokenType.NUMBER, float(self.source[self.start:self.current]))
+
+    def _add_token(self, token_type, value=None):
+        self.tokens.append(Token(token_type, value))
+
+    def _peek(self):
+        if self._is_at_end():
+            return "\0"
+        return self.source[self.current]
+
+    def _peek_next(self):
+        if self.current + 1 >= len(self.source):
+            return "\0"
+        return self.source[self.current + 1]
+
+    def _advance(self):
+        self.current += 1
+        return self.source[self.current - 1]
+
+    def _is_at_end(self):
+        return self.current >= len(self.source)
---
+++ tests/test_scanner.py
@@ -0,0 +1,76 @@
+from calculator.scanner import Scanner, TokenType
+
+
+def test_scanner_can_tokenize_a_plus():
+    source = "+"
+
+    token = Scanner(source).scan_tokens()[0]
+
+    assert token.type == TokenType.PLUS
+
+
+def test_scanner_can_tokenize_a_minus():
+    source = "-"
+
+    token = Scanner(source).scan_tokens()[0]
+
+    assert token.type == TokenType.MINUS
+
+
+def test_scanner_can_tokenize_a_times():
+    source = "*"
+
+    token = Scanner(source).scan_tokens()[0]
+
+    assert token.type == TokenType.TIMES
+
+
+def test_scanner_can_tokenize_a_divide():
+    source = "/"
+
+    token = Scanner(source).scan_tokens()[0]
+
+    assert token.type == TokenType.DIVIDE
+
+
+def test_scanner_can_tokenize_a_left_paren():
+    source = "("
+
+    token = Scanner(source).scan_tokens()[0]
+
+    assert token.type == TokenType.LEFT_PAREN
+
+
+def test_scanner_can_tokenize_a_right_paren():
+    source = ")"
+
+    token = Scanner(source).scan_tokens()[0]
+
+    assert token.type == TokenType.RIGHT_PAREN
+
+
+def test_scanner_can_tokenize_a_digit():
+    source = "1"
+
+    token = Scanner(source).scan_tokens()[0]
+
+    assert token.type == TokenType.NUMBER
+    assert token.value == 1
+
+
+def test_scanner_can_tokenize_multiple_digits():
+    source = "123"
+
+    token = Scanner(source).scan_tokens()[0]
+
+    assert token.type == TokenType.NUMBER
+    assert token.value == 123
+
+
+def test_scanner_can_tokenize_numbers_with_fractional_parts():
+    source = "123.456"
+
+    token = Scanner(source).scan_tokens()[0]
+
+    assert token.type == TokenType.NUMBER
+    assert token.value == 123.456
